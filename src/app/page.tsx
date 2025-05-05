4c566d174ea621c914cf4ba4656fd9f654710a1a
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-05-05 12:21:59 +0700

    Update asset disamain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Schedule from "../Components/Schedule";
 import Logos from "../Components/logos";
 import MapLocation from '../Components/MapLocation';
 import AssetOne from "../Images/Asset 1.svg";
-import AssetFour from "../Images/Asset 4.svg"
+// import AssetFour from "../Images/Asset 4.svg"
 import backgroundOweek from "./../Images/oweek_background.png";
 
 import DisableZoom from "./../Scripts/DisableZoom";
@@ -31,8 +31,8 @@ export default function Home() {
             <div className="pt-10"></div>
             {/* original gradient color : bg-gradient-to-b from-[#f9a817] via-[#e77a21] to-[#d76b12] */}
             <div className="relative bg-[#e77a21] w-screen p-20">
-              <Image className="absolute top-0 right-0" width={500} src={AssetOne} alt="Description of Asset One" draggable='false'/>
-              <Image className="absolute bottom-0 left-0" width={200} src={AssetFour} alt="Description of Asset One" draggable='false'/>
+              <Image className="absolute top-0 right-0" width={400} src={AssetOne} alt="Description of Asset One" draggable='false'/>
+              <Image className="absolute bottom-0 left-0 rotate-180" width={400} src={AssetOne} alt="Description of Asset One" draggable='false'/>
               <Schedule />
               <div className="pt-5"></div>
               <MapLocation/>
